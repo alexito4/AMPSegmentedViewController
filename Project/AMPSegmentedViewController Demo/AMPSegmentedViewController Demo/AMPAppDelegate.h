2a45cb6ef90c2c09acf33950b836a50d5f7e011e
Alejandro Martinez <alexito4@gmail.com>
2013-09-21 14:33:34 +0200

    Initial Commit

---
+++ Project/AMPSegmentedViewController Demo/AMPSegmentedViewController Demo/AMPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AMPAppDelegate.h
+//  AMPSegmentedViewController Demo
+//
+//  Created by Alejandro Martinez on 21/09/13.
+//  Copyright (c) 2013 Alejandro Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
